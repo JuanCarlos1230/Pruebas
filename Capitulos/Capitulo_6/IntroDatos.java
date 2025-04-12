a907b961afaef90fa37ad4cc9fce5b0bf5b62f83
Juan Carlos <carlos85347@gmail.com>
2025-04-12 07:55:08 +0200

    Scanner

---
+++ Capitulos/Capitulo_6/IntroDatos.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class IntroDatos{
+    public static void main(String[] args){
+        
+        Scanner texto = new Scanner(System.in);
+        String nombre = "";
+        int numUno = 0, numDos = 0, resultado = 0;
+        
+        System.out.println("Cual és tu nombre?");
+        nombre = texto.nextLine(); //guarda texto el metodo nextLine
+        
+        System.out.println("Dame el primer valor para tu suma:");
+        numUno = texto.nextInt(); //guarda números el metodo nextInt         
+        
+        System.out.println("Dame el segundo valor para tu suma:");
+        numDos = texto.nextInt(); //guarda números el metodo nextInt
+        
+        resultado = numUno + numDos;
+        
+        System.out.println("Hola " + nombre + " el resultado de tu suma és " + resultado);  
+    }
+}
+
