07b4406f5a5bc9c2ce87b4b49a9ad60807c24e08
dev <dev@introprg.myguest.virtualbox.org>
2025-06-18 16:22:28 +0200

    Añadiendo comentario y cambiando fin de bucle

--- Capitulos/12.Do_While/CuentAtras.java
+++ Capitulos/12.Do_While/CuentAtras.java
@@ -1,12 +1,20 @@
 
+/* Programa que forma un bucle decreciente desde el 1000 al 200
+ *disminuyendo cada vez 200. 
+ */ 
+ 
 public class CuentAtras{
     public static void main(String[] args){
         int i = 1000;        
-        do {
-            System.out.print(i + ", ");
-            i-=200;
-        } while(i>=200);
-            System.out.println(i);       
+        do {  
+            if (i > 200){
+                System.out.print(i + ", ");
+                i-=200;
+            }
+            if (i == 200){
+                System.out.println(i);  
+            }         
+        } while(i>200);    
     }
 }
 
