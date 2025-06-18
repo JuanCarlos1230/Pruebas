6f190f81fe82b3e069984bc2a6cc5ac878318de6
dev <dev@introprg.myguest.virtualbox.org>
2025-06-18 11:47:24 +0200

    Comentario añadido

--- Capitulos/07.Condicionales_Logicos/CondicionalesLogicos.java
+++ Capitulos/07.Condicionales_Logicos/CondicionalesLogicos.java
@@ -1,4 +1,6 @@
-
+/* Programa que determina los dias de vacaciones que te pertenecen segun tus años de
+servicio en la empresa.*/
+ 
 import java.util.Scanner;
 
 public class CondicionalesLogicos{
@@ -8,17 +10,19 @@ public class CondicionalesLogicos{
         int departamento = 0, antiguedad = 0;
         Scanner clave = new Scanner(System.in);
         System.out.println("Bienvenido al sistema vacacional de Coca-Cola Company.");
+        System.out.println();
         System.out.println("Cuál es el nombre del trabajador?");
         /* con Scanner podemos ingresar numero o textos solo tendremos que 
         indicar nextLine para texto o nextInt para números*/
         nombre = clave.nextLine(); // guarda el nombre con el metodo nextLine        
         System.out.println("A que departamento perteneces?");       
-        departamento = clave.nextInt(); //guarda números el metodo nextInt
+        departamento = clave.nextInt(); // guarda números el metodo nextInt
          
         if (departamento == 1){
             System.out.println("Tu departamento pertenece a la Aténción del cliente.");    
+            System.out.println();
             System.out.println("Cuántos años de servicio tiene el trabajador?");
-            antiguedad = clave.nextInt(); //guarda números el metodo nextInt
+            antiguedad = clave.nextInt(); // guarda números el metodo nextInt
             if (antiguedad < 0){
                 System.out.println("No puede haber años negativos.");
             } else if (antiguedad < 1){
@@ -32,6 +36,7 @@ public class CondicionalesLogicos{
             }
         } else if (departamento == 2){
             System.out.println("Tu departamento pertenece a la Logística.");    
+            System.out.println();
             System.out.println("Cuántos años de servicio tiene el trabajador?");
             antiguedad = clave.nextInt(); //guarda números el metodo nextInt
             if (antiguedad < 0){
@@ -47,6 +52,7 @@ public class CondicionalesLogicos{
             }
         } else if (departamento == 3){
             System.out.println("Tu departamento pertenece a la Gerencia.");    
+            System.out.println();
             System.out.println("Cuántos años de servicio tiene el trabajador?");
             antiguedad = clave.nextInt(); //guarda números el metodo nextInt
             if (antiguedad < 0){
