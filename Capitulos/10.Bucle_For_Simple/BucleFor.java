8b3796fefc2bfc93b4f14377f1f4a4993ded6c3f
dev <dev@introprg.myguest.virtualbox.org>
2025-06-18 15:22:18 +0200

    Comentario añadido

--- Capitulos/10.Bucle_For_Simple/BucleFor.java
+++ Capitulos/10.Bucle_For_Simple/BucleFor.java
@@ -1,10 +1,15 @@
-
+// Programa que forma un bucle imprrimiendo 1,2,3,4,5
+  
 public class BucleFor{
     public static void main(String[] args){        
-        for(int i = 1; i <=4; i++){
-            System.out.print(i + ",");
-        }
-        System.out.println(5);            
+        for(int i = 1; i <= 5; i++){
+            if (i >=1 && i <= 4){
+                System.out.print(i + ",");
+            }
+            if (i == 5){
+                System.out.println(i);
+            }
+        }            
     }
 }
 
