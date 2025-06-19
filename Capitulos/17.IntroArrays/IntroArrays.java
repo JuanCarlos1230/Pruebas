617bb3799ecc11bd444bc5d43b2378bc4cf30bff
dev <dev@introprg.myguest.virtualbox.org>
2025-06-19 17:37:27 +0200

    Añadiendo comentarios

--- Capitulos/17.IntroArrays/IntroArrays.java
+++ Capitulos/17.IntroArrays/IntroArrays.java
@@ -1,3 +1,8 @@
+
+/* Programa quwe pregunta al usuario cuantos numeros desea guardar en el array y 
+despues le muestra los numero guardados.
+*/
+
 import java.util.Scanner;
 
 public class IntroArrays{
@@ -6,19 +11,31 @@ public class IntroArrays{
         Scanner num = new Scanner(System.in);
         // Preguntamos al usuario cuantos numeros desea guardar en el array
         System.out.print("Cuantos números deseas guardar en el array? ");   
-        int quants = num.nextInt();   
+        int quants = num.nextInt();
+        
+        if (quants <= 0) {
+            System.out.println("Cuantos números deseas guardar en el array debe ser mayor a 0."); 
+            return;   
+        }
+           
         int nums[] = new int[quants]; 
         int i = 0;
-        int numeros = nums[i];       
+        int numeros = nums[i];
+               
         for (i = 0; i < nums.length; i++){ 
             System.out.print("Introduzca el número " + (i + 1) + ": ");    
             nums[i] = num.nextInt();
             numeros = nums[i];
-        }    
-        System.out.print("Los numero guardados son: ");
-        for (int j = 0; j < nums.length; j++){
-            System.out.print("[" + nums[j] + "]");              
         }
+            
+        System.out.print("Los numero guardados son: ");
+        System.out.print("[");
+        int j = 0;
+        for (j = 0; j < nums.length-1; j++){
+            System.out.print(nums[j] + ",");              
+        }
+            
+        System.out.print(nums[j] + "]");
         System.out.println();              
     }
 }
